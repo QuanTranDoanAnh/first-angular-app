8bfe6753460aba40040c140b100042b893d07272
QuanTranDoanAnh <td.anhquan@gmail.com>
2024-11-10 14:50:44 +0700

    Input Signal

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -11,12 +11,17 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true}) avatar !: string;
-  @Input({ required:true}) name !: string;
+  // @Input({ required: true}) avatar !: string;
+  //@Input({ required:true}) name !: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  })
 
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {
     
