fefabad7e33e09e5999166a785458019d0d80010
QuanTranDoanAnh <td.anhquan@gmail.com>
2024-11-10 15:55:04 +0700

    using output() function

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, computed, EventEmitter, input, Input, output, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
@@ -11,16 +11,23 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id !: string;
-  @Input({ required: true}) avatar !: string;
-  @Input({ required:true}) name !: string;
-  @Output() select = new EventEmitter();
-  
-  get imagePath() {
-    return 'assets/users/' + this.avatar;
-  }
+  //@Input({ required: true }) id !: string;
+  //@Input({ required: true}) avatar !: string;
+  //@Input({ required:true}) name !: string;
+  //@Output() select = new EventEmitter();
+  id = input.required<string>();
+  avatar = input.required<string>();
+  name = input.required<string>();
+  select = output<string>();
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar(); 
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.avatar;
+  // }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.id());
   }
 }
